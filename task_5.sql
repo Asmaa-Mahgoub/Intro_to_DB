fb95837c47b67fe42e978208c5944a622c2ecaca
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-08-18 01:50:41 +0300

    Populating Your Database with Your Very First Data

--- task_5.sql
+++ task_5.sql
@@ -12,7 +12,7 @@ mycursor.execute("DELETE FROM customer WHERE customer_id = 1")
 mydb.commit()
 
 mycursor.execute("""
-INSERT INTO customer (customer_id, customer_name, email, address)
+INSERT INTO customer(customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'
 )
 """)
