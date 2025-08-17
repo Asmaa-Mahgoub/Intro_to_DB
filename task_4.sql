7e0e13682c4c828444296c8f4921f49f9020f9db
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-08-18 01:18:35 +0300

    Write a script that prints the full description of the table books from the database alx_book_store in your MySQL server.

---
+++ task_4.sql
@@ -0,0 +1,26 @@
+import mysql.connector
+import sys
+
+try:
+    mydb= mysql.connector.connect(
+    host="localhost",
+    user= "asmaa_mahgoub",
+    password= "Asmaa1239",
+    database= "alx_book_store"
+)
+
+    mycursor= mydb.cursor()
+    mycursor.execute("""
+        SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_NAME = 'Books'
+    """)
+    for row in mycursor.fetchall():
+        print(f"{row[0]} | {row[1]} | {row[2]} | {row[3]}")
+
+except mysql.connector.Error as e:
+    print(f"Error: {e}")
+
+finally:
+        mycursor.close()
+        mydb.close()
