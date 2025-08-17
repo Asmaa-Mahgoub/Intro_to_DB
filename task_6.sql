f783a46010a0e02aeb7894e42cd32d2e8c9ca54f
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-08-18 01:48:38 +0300

    Write a script that inserts multiple rows in the table customer (database alx_book_store) in your MySQL server.

--- task_6.sql
+++ task_6.sql
@@ -22,9 +22,9 @@ try:
     VALUES (%s, %s, %s, %s)
     """
     values = [
-        (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
-        (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
-        (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.")
+        (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."),
+        (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
+        (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.")
     ]
 
    
