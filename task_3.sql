ed73b222cea4a4c188402f4461b177cdae18e852
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-08-18 00:43:20 +0300

    List all the tables created

--- task_3.sql
+++ task_3.sql
@@ -1,18 +1,43 @@
+# task_3.py
 import mysql.connector
+import sys
 
-mydb= mysql.connector.connect(
-    host="localhost",
-    user= "asmaa_mahgoub",
-    password= "Asmaa1239",
-    database= "alx_book_store"
-)
+# Get database name from command-line argument
+if len(sys.argv) < 2:
+    print("Usage: python task_3.py <database_name>")
+    sys.exit()
 
-mycursor= mydb.cursor()
-mydb.commit()
+database_name = sys.argv[1]
 
-def list_book():
-    sql= "SELECT * FROM Books"
-    mycursor.execute(sql)
+try:
+    # Connect to MySQL server
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="asmaa_mahgoub",   # your MySQL username
+        password="Asmaa1239"    # your MySQL password
+    )
 
-mycursor.close()
-mydb.close()
+    # Create a cursor to execute SQL commands
+    mycursor = mydb.cursor()
+
+    # Select the database
+    mycursor.execute(f"USE {database_name}")
+
+    # Execute SHOW TABLES to list all tables
+    mycursor.execute("SHOW TABLES")
+    tables = mycursor.fetchall()
+
+    # Print the table names
+    print(f"Tables in '{database_name}':")
+    for table in tables:
+        print(table[0])
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
+finally:
+    # Close cursor and connection
+    if 'mycursor' in locals():
+        mycursor.close()
+    if 'mydb' in locals() and mydb.is_connected():
+        mydb.close()
